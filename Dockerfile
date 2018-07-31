f9a3d303e7345547c7ae2c40ec07272a4988d103
Evgeniy Tolstolutskyi <evgeniy.tolstolutskyi@nixsolutions.com>
2018-07-31 15:39:48 +0300

    added project configuration and created database structure

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:8-jdk-alpine
+ADD /target/bookstore-*.jar /
+RUN echo "networkaddress.cache.ttl=0" >> /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/java.security
+
+EXPOSE 8080
+ENTRYPOINT java -Dspring.profiles.active=${SPA} -Dnetworkaddress.cache.ttl=0 -jar bookstore-*.jar
---
+++ src/main/java/com/tolstolutskyi/BookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.tolstolutskyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class BookStoreApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookStoreApplication.class);
+    }
+}
